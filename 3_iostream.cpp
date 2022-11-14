5e71b14776f7ad7878eb3747d98c5d8e3362ce9e
Chansik Yun <chansigi@gmail.com>
2022-11-14 11:08:26 +0900

    .

--- 3_iostream.cpp
+++ 3_iostream.cpp
@@ -9,19 +9,25 @@ using namespace std;
 #if 0
 int main()
 {
-  int age;
+  double age;
   printf("age: ");
 
-  scanf("%d", &age);
-  printf("age: %d\n", age);
+  scanf("%lf", &age);
+  printf("age: %lf\n", age);
 }
 #endif
 
+// 1. 서식을 지정하지 않고도, 각 타입에 따른 입력/출력이 제대로 처리됩니다.
+// 2. 주소를 전달하지 않아도, 변수의 값이 변경됩니다.
+// 3. std::cout / std::cin / std::endl 정체는 무엇인가?
+
+#if 1
 int main()
 {
-  int age;
+  double age;
   cout << "age: ";
 
   cin >> age;
   cout << "age: " << age << endl;
 }
+#endif
