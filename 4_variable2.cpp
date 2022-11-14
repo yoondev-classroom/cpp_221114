e04a264bfcce3a1b9982820e4fc0212b976d99c4
Chansik Yun <chansigi@gmail.com>
2022-11-14 11:40:53 +0900

    .

--- 4_variable2.cpp
+++ 4_variable2.cpp
@@ -25,6 +25,7 @@ int main()
 }
 #endif
 
+#if 0
 // C++11 - Uniform Initialization(일관된 초기화)
 int main()
 {
@@ -37,4 +38,39 @@ int main()
   int n2{10};
   int x2[3]{1, 2, 3};
   Point p2{10, 20};
+}
+#endif
+
+int main()
+{
+  double d = 3.14;
+
+  int n1 = d;
+  int n2(d);
+
+  // int n3 = {d}; // Compile error!
+  // int n4{d};    // Compile error!
+
+  // - Uniform Initialization을 사용하면 컴파일 타임에 암묵적인 변환을
+  //   금지할 수 있습니다.
+  // => 명시적인 캐스팅이 필요합니다.
+  // => Preventing Narrow 기능을 제공합니다.
+
+  int n3 = {(int)d};
+  int n4{(int)d};
+
+  cout << n1 << endl;
+  cout << n2 << endl;
+  cout << n3 << endl;
+  cout << n4 << endl;
+
+  long long l = INT_MAX + 1LL;
+
+  int x1 = l;
+  int x2(l);
+  cout << x1 << endl;
+  cout << x2 << endl;
+
+  int x3 = {(int)l};
+  int x4{(int)l};
 }
