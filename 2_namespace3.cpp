9ce718adb7d97331104723d67f8f9ba6572df4d4
Chansik Yun <chansigi@gmail.com>
2022-11-14 10:37:51 +0900

    .

--- 2_namespace3.cpp
+++ 2_namespace3.cpp
@@ -8,6 +8,15 @@
 
 // #include <stdio.h>
 #include <cstdio>
+#if 0
+
+namespace std 
+{
+#include <stdio.h>
+}
+
+#endif
+
 // 1. C++ 표준 헤더는 확장자가 없는 경우가 많습니다.
 // 2. C의 모든 표준 함수를 std 이름 공간안에서 사용할 수 있습니다.
 
