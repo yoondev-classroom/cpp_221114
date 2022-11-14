890aa8d4b384616f3168782bb66ebc60dbd6745b
Chansik Yun <chansigi@gmail.com>
2022-11-14 14:32:05 +0900

    .

---
+++ 9_함수.cpp
@@ -0,0 +1,27 @@
+// 9_함수.cpp
+
+#include <iostream>
+using namespace std;
+
+// 기본 파라미터
+//  정의: 함수의 파라미터의 기본값을 지정할 수 있습니다.
+//  => 함수에 전달된 인자가 없는 경우, 컴파일러가 기본값을 자동으로 전달합니다.
+
+int add(int a, int b, int c = 0, int d = 0)
+{
+  return a + b + c + d;
+}
+
+int main()
+{
+  int result = 0;
+
+  result = add(10, 20, 30, 40);
+  cout << result << endl;
+
+  result = add(10, 20, 30); // result = add(10, 20, 30, 0);
+  cout << result << endl;
+
+  result = add(10, 20); // result = add(10, 20, 0, 0);
+  cout << result << endl;
+}
