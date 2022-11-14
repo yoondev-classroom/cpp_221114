75f55f690cde7f32fa41e4bf9630bc1a58d44c21
Chansik Yun <chansigi@gmail.com>
2022-11-14 13:27:52 +0900

    .

--- 5_using.cpp
+++ 5_using.cpp
@@ -14,16 +14,39 @@ using namespace std;
 // 2. C++11, 기존 타입의 별칭을 만들 때 사용할 수 있습니다.
 //   => typedef를 대체합니다.
 
-foo()
+typedef int ARR3[3];
+
+int *foo()
 {
-  static int x[3];
-  return x;
+  // static int x[3];
+  static ARR3 x;
+
+  return x; // &x[0];
 }
 
-goo()
+// int (*goo())[3]
+
+// 1. goo()
+// - goo는 함수 입니다. 함수의 반환타입은?
+
+// 2. (*goo())
+// - 함수의 반환타입은 포인터 입니다. 어떤 주소를 가르키는가?
+
+// 3. (*goo())[3]
+// - 3개짜리 배열의 주소를 가르킵니다. 배열의 각 요소는?
+
+// 4. int (*goo())[3]
+// - 배열의 각 요소는 int 입니다.
+
+typedef int (*PARR3)[3];
+
+// int (*goo())[3]
+// ARR3 *goo()
+
+PARR3 goo()
 {
   static int x[3];
-  return &x;
+  return &x; // int(*)[3]
 }
 
 int main()
