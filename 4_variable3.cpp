85d3461826fe33d24522e68904fafe6b265e7496
Chansik Yun <chansigi@gmail.com>
2022-11-14 13:08:36 +0900

    .

--- 4_variable3.cpp
+++ 4_variable3.cpp
@@ -22,7 +22,7 @@ int main()
 //  1) 배열에서는 사용 안됩니다.
 //  2) 반드시 초기화가 필요합니다.
 //  3) 우항의 표현식과 동일한 타입이 아니라, 추론입니다.
-
+#if 0
 int main()
 {
   int x[3] = {10, 20, 30};
@@ -38,4 +38,27 @@ int main()
 
   // y = 100; /* Compiler error */
   cout << y << endl;
+}
+#endif
+
+// 변수와 동일한 타입을 구하는 연산자
+// => decltype, C++11
+// => 초기화 없이도 사용할 수 있습니다.
+// => 정확한 타입으로 나옵니다.
+
+// GCC 확장 문법: __typeof
+//  => decltype과 동일합니다.
+
+int main()
+{
+  const int c = 100;
+  auto n = c; // n -> int
+  n = 100;
+  cout << n << endl;
+
+  decltype(c) c2 = 200; // decltype(c) -> const int
+  // c2 = 100; // Compile Error!
+  cout << c2 << endl;
+
+  decltype(n) n2; // decltype(n) -> int
 }
