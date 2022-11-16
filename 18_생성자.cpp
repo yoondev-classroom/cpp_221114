9f977e0284beb14230bf7c51b3bd9dd572c346bf
Chansik Yun <chansigi@gmail.com>
2022-11-16 11:39:03 +0900

    .

--- 18_생성자.cpp
+++ 18_생성자.cpp
@@ -9,6 +9,12 @@ using namespace std;
 // 2. 사용자가 생성자를 1개라도 제공하면,
 //   컴파일러는 기본 생성자를 제공하지 않습니다.
 
+// 3. 생성자는 오버로딩이 가능합니다.
+//  => 인자의 타입과 개수에 따라 생성자를 여러개 제공할 수 있습니다.
+//   "객체를 생성하는 다양한 방법을 제공할 수 있습니다."
+
+// 4. 클래스의 기본 생성자를 제공하는 것이 편리합니다.
+
 class Point
 {
   int x;
@@ -18,7 +24,7 @@ public:
   // 컴파일러가 만드는 생성자 형태
   // Point() {}
 
-#if 0
+#if 1
   Point()
   {
     cout << "Point()" << endl;
@@ -26,6 +32,7 @@ public:
     y = 0;
   }
 #endif
+
   Point(int a, int b)
   {
     cout << "Point(int, int)" << endl;
@@ -41,7 +48,8 @@ public:
 
 int main()
 {
-  // Point p1; /* Compile Error */
+  // Point p1; /* Compile Error */ => 기본 생성자가 없는 경우
+  Point p2;
 
   Point p1(10, 20);
 
