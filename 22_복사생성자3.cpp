03165776543581cd8ca8f847099766cbd84cd85d
Chansik Yun <chansigi@gmail.com>
2022-11-16 15:12:58 +0900

    .

---
+++ 22_복사생성자3.cpp
@@ -0,0 +1,33 @@
+// 22_복사생성자3.cpp
+#include <iostream>
+using namespace std;
+
+class Sample
+{
+public:
+  Sample() {}
+  Sample(const Sample &rhs) { cout << "Sample(const&)" << endl; }
+  ~Sample() { cout << "~Sample()" << endl; }
+};
+
+// 복사 생성자가 호출되는 경우
+// 1) 객체가 함수의 인자로 값으로 전달될 때
+#if 0
+void foo(Sample x) // x: Sample(const&)
+{
+  // 읽기
+}
+// x: ~Sample()
+#endif
+
+// 해결 방법: 객체를 call by value로 전달할 때는
+//          같은 의도를 가지고 있는, const&를 사용해야 합니다.
+void foo(const Sample &x)
+{
+}
+
+int main()
+{
+  Sample s;
+  foo(s);
+}
