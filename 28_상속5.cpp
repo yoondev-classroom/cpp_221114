9e6f547aa7003f327027a59f1e12fb0495f7df3a
Chansik Yun <chansigi@gmail.com>
2022-11-17 14:33:32 +0900

    .

--- 28_상속5.cpp
+++ 28_상속5.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 using namespace std;
 
+#if 0
 class Base
 {
 public:
@@ -30,4 +31,90 @@ public:
 int main()
 {
   Derived d;
+}
+#endif
+
+#if 0
+class Base
+{
+private:
+  int ba_;
+  int bb_;
+
+public:
+  Base(int ba, int bb) : ba_(ba), bb_(bb) {}
+};
+
+class Derived : public Base
+{
+  int a_;
+  int b_;
+
+public:
+  // 자식의 생성자는 반드시 부모의 생성자를 초기화하는데 필요한 인자도 전달받아야 합니다.
+  Derived(int ba, int bb, int a, int b)
+      : Base(ba, bb), a_(a), b_(b)
+  {
+  }
+};
+#endif
+
+#if 0
+class Student
+{
+  string name;
+  string address;
+  int id;
+
+public:
+  Student(const string &n, const string &a, int i)
+      : name(n), address(a), id(i)
+  {
+  }
+
+  const string &GetName() const { return name; }
+  void SetName(const string &s) { name = s; }
+
+  const string &GetAddress() const { return address; }
+  void SetAddress(const string &s) { address = s; }
+
+  int GetId() const { return id; }
+  void SetId(int i) { id = i; }
+};
+#endif
+
+class Person
+{
+  string name;
+  string address;
+
+public:
+  Person(const string &n, const string &a)
+      : name(n), address(a)
+  {
+  }
+
+  const string &GetName() const { return name; }
+  void SetName(const string &s) { name = s; }
+
+  const string &GetAddress() const { return address; }
+  void SetAddress(const string &s) { address = s; }
+};
+
+class Student : public Person
+{
+  int id;
+
+public:
+  Student(const string &n, const string &a, int i)
+      : Person(n, a), id(i)
+  {
+  }
+
+  int GetId() const { return id; }
+  void SetId(int i) { id = i; }
+};
+
+int main()
+{
 }
