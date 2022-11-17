48bfd0316cdc0f33f9f197c5dc754b471e2bccbc
Chansik Yun <chansigi@gmail.com>
2022-11-17 16:45:28 +0900

    .

--- 29_다형성8.cpp
+++ 29_다형성8.cpp
@@ -22,6 +22,13 @@ using namespace std;
 // 5. 부모 클래스의 소멸자는 반드시 가상이어야 한다.
 //    => 가상 소멸자
 
+// 6. OCP(Open Close Principle, 개방 폐쇄 원칙)
+//   : 확장에는 열려 있고, 수정에는 닫혀 있어야 합니다.
+//   => "새로운 기능이 추가되어도 기존 코드는 수정되면 안된다."
+
+// 7. 상속은 재사용이 아닌, 다형성을 목적으로 사용해야 합니다.
+//   => Upcasting, Overriding, virtual
+
 class Shape
 {
 public:
@@ -41,6 +48,12 @@ class Circle : public Shape
 {
 public:
   void Draw() const override { cout << "Circle Draw" << endl; }
+};
+
+class Triangle : public Shape
+{
+public:
+  void Draw() const override { cout << "Triangle Draw" << endl; }
 };
 
 int main()
@@ -63,11 +76,21 @@ int main()
     {
       shapes.push_back(new Circle);
     }
+    else if (cmd == 3)
+    {
+      shapes.push_back(new Triangle);
+    }
     else if (cmd == 9)
     {
       for (auto e : shapes)
       {
         e->Draw();
+        // e의 실제 객체 타입에 따라서 각 클래스의 함수가 호출됩니다.
+        // Draw() ----> Rect::Draw()
+        //              Circle::Draw()
+        //              Triangle::Draw()
+        // - 다형성(Polymorphism)
+        //  "OCP를 만족합니다."
       }
     }
     else if (cmd == 0)
