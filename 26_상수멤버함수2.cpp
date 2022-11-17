9041794c77b73c3d436cfbff2116f8a6ae35782a
Chansik Yun <chansigi@gmail.com>
2022-11-17 13:07:27 +0900

    .

--- 26_상수멤버함수2.cpp
+++ 26_상수멤버함수2.cpp
@@ -35,8 +35,19 @@ public:
 
     return leftTop;
   }
+
+  // C++은 상수 멤버 함수와 일반 멤버 함수를 동시에 제공할 수 있습니다.
+  Point &GetLeftTop()
+  {
+    return leftTop;
+  }
 };
 
 int main()
 {
+  Rect rect(10, 20);
+  Point &p = rect.GetLeftTop();
+
+  p.Set(100, 200);
+  p.Print();
 }
