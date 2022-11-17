5d78cf86b839844bc6b644a6c9f8eb19b80b79de
Chansik Yun <chansigi@gmail.com>
2022-11-17 14:47:04 +0900

    .

--- 28_상속6.cpp
+++ 28_상속6.cpp
@@ -3,6 +3,7 @@
 #include <string>
 using namespace std;
 
+#if 0
 class Cat
 {
   int age_;
@@ -34,6 +35,60 @@ public:
 
   int GetAge() const { return age_; }
   string GetName() const { return name_; }
+};
+
+//---
+// => 아래의 코드는 수정없이 동작해야 합니다.
+int main()
+{
+  Dog d(42, 100, "Navi");
+  Cat c(255, 100, "Hello");
+
+  cout << d.GetAge() << endl;
+  cout << d.GetName() << endl;
+
+  cout << c.GetAge() << endl;
+  cout << c.GetColor() << endl;
+  cout << c.GetName() << endl;
+}
+#endif
+
+class Animal
+{
+  int age_;
+  string name_;
+
+public:
+  Animal(int age, const string &name)
+      : age_(age), name_(name) {}
+
+  int GetAge() const { return age_; }
+  string GetName() const { return name_; }
+};
+
+class Cat : public Animal
+{
+
+  int color_;
+
+public:
+  Cat(int age, int color, const string &name)
+      : Animal(age, name), color_(color)
+  {
+  }
+
+  int GetColor() const { return color_; }
+};
+
+class Dog : public Animal
+{
+  int speed_;
+
+public:
+  Dog(int age, int speed, const string &name)
+      : Animal(age, name), speed_(speed)
+  {
+  }
 };
 
 //---
