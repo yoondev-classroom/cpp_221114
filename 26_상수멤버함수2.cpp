cf2bffcfb17a25df4edf3c693ab61fcb72d5b2f7
Chansik Yun <chansigi@gmail.com>
2022-11-17 13:12:27 +0900

    .

--- 26_상수멤버함수2.cpp
+++ 26_상수멤버함수2.cpp
@@ -32,16 +32,32 @@ public:
   const Point &GetLeftTop() const
   {
     // leftTop: const Point
-
+    cout << "상수 멤버 함수" << endl;
     return leftTop;
   }
 
   // C++은 상수 멤버 함수와 일반 멤버 함수를 동시에 제공할 수 있습니다.
   Point &GetLeftTop()
   {
+    cout << "일반 멤버 함수" << endl;
     return leftTop;
   }
 };
+
+// 상수 멤버 함수만 제공하는 경우
+//        Sample s; -> 상수 멤버 함수
+//  const Sample s; -> 상수 멤버 함수
+// const Sample& s; -> 상수 멤버 함수
+
+// 상수 멤버 함수와 비상수 멤버 함수를 동시에 제공하는 경우
+//        Sample s; -> 비상수 멤버 함수
+//  const Sample s; -> 상수 멤버 함수
+// const Sample& s; -> 상수 멤버 함수
+
+// 비상수 멤버 함수만 제공하는 경우
+//        Sample s; -> 비상수 멤버 함수
+//  const Sample s; -> X
+// const Sample& s; -> X
 
 int main()
 {
@@ -50,4 +66,16 @@ int main()
 
   p.Set(100, 200);
   p.Print();
+
+  const Rect rect2(10, 20);
+  const Point &p2 = rect2.GetLeftTop();
+
+  // p.Set(100, 200);
+  p2.Print();
+
+  const Rect &r = rect;
+  const Point &p3 = r.GetLeftTop();
+
+  // p3.Set(100, 200);
+  p3.Print();
 }
