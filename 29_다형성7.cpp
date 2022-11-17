32529a0148a31cfca7882593c5cf4ac045514619
Chansik Yun <chansigi@gmail.com>
2022-11-17 16:25:09 +0900

    .

---
+++ 29_다형성7.cpp
@@ -0,0 +1,40 @@
+// 29_다형성7.cpp
+
+#include <iostream>
+using namespace std;
+
+// 가상 소멸자
+//  : 부모 클래스의 소멸자는 반드시 가상이어야 합니다.
+
+class Animal
+{
+public:
+  Animal() { cout << "Animal()" << endl; }
+  virtual ~Animal() { cout << "~Animal()" << endl; }
+};
+
+class Dog : public Animal
+{
+public:
+  Dog() { cout << "Dog()" << endl; }
+  ~Dog() { cout << "~Dog()" << endl; }
+};
+
+// 문제점: 자식의 소멸자가 호출되지 않아서, 메모리 누수의 위험성이 있습니다.
+//  원인: 소멸자가 정적 바인딩 기반으로 호출되기 때문입니다.
+//    p의 타입이 부모 타입일 경우, 자식의 소멸자가 제대로 호출되지 않습니다.
+
+int main()
+{
+  // Dog d;
+  //  Dog::Dog()
+  //  Dog::~Dog()
+
+  Animal *p = new Dog;
+  // Dog::Dog()
+
+  delete p;
+  // 가상 소멸자가 아닌 경우, Animal::~Animal()
+  // 가상 소멸자인 경우는 실행 시간에 p의 객체의 실제 타입을 조사해서
+  // 소멸자를 호출합니다. => Dog::~Dog()
+}
