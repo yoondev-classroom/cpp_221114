66830e023b099b39c7a66e5c655291b6ebeaa90e
Chansik Yun <chansigi@gmail.com>
2022-11-17 16:16:31 +0900

    .

---
+++ 29_다형성6.cpp
@@ -0,0 +1,30 @@
+// 29_다형성6.cpp
+#include <iostream>
+using namespace std;
+
+class Animal
+{
+public:
+  virtual void Go() { cout << "Animal Go" << endl; }
+};
+
+//  C++11에서 오버라이딩 금지를 위한 키워드가 도입되었습니다.
+//   => final
+class Dog : public Animal
+{
+public:
+  // Dog의 자식 클래스는 더 이상 Go를 오버라이딩 금지하고 싶다.
+  void Go() override final { cout << "Dog Go" << endl; }
+};
+
+class Pomeranian : public Dog
+{
+public:
+  // void Go() override { cout << "Pomeranian Go" << endl; }
+};
+
+int main()
+{
+  Animal *p = new Pomeranian;
+  p->Go();
+}
