454bc5bf3f9cebb4421c0237ccd4ba0607273c04
Chansik Yun <chansigi@gmail.com>
2022-11-17 09:32:46 +0900

    .

--- 20_this.cpp
+++ 20_this.cpp
@@ -35,7 +35,7 @@ struct Point
   // this는 멤버 함수 내부에서 사용할 수 있는 전달 받은 객체의 주소를
   // 의미합니다.
 
-  // void Set(Point *const this, int ax, int ay) => thiscall
+  // void Point::Set(Point *const this, int ax, int ay) => thiscall
   void Set(int ax, int ay)
   {
     this->x = ax;
@@ -76,6 +76,7 @@ class Sample
   int x;
 
 public:
+  // void Sample::Set(Sample* this, int a, int b)
   void Set(int a, int b)
   {
     this->x = a + b;
@@ -83,9 +84,15 @@ public:
   }
   /*
 void Sample::Set(int,int) PROC                   ; Sample::Set, COMDAT
-        mov     DWORD PTR [rsp+24], r8d
-        mov     DWORD PTR [rsp+16], edx
-        mov     QWORD PTR [rsp+8], rcx
+        mov     DWORD PTR [rsp+24], r8d          // b = 200
+        mov     DWORD PTR [rsp+16], edx          // a = 100
+        mov     QWORD PTR [rsp+8], rcx           // this = &s
+        mov     eax, DWORD PTR b$[rsp]           // eax = b
+        mov     ecx, DWORD PTR a$[rsp]           // ecx = a
+        add     ecx, eax                         // ecx += eax
+        mov     eax, ecx                         // eax = ecx
+        mov     rcx, QWORD PTR this$[rsp]        // rcx = this;
+        mov     DWORD PTR [rcx], eax             // this->x = eax;
         ret     0
 void Sample::Set(int,int) ENDP                   ; Sample::Set
   */
