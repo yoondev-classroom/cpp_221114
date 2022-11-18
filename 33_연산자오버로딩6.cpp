0746d1c184cb2a52b6ad9c168e7dc8d4585a4893
Chansik Yun <chansigi@gmail.com>
2022-11-18 13:34:22 +0900

    .

--- 33_연산자오버로딩6.cpp
+++ 33_연산자오버로딩6.cpp
@@ -7,11 +7,11 @@ class Integer
   int value;
 
 public:
-  Integer(int n = 0)
+  inline Integer(int n = 0)
       : value(n) {}
 
   // ++n
-  Integer &operator++()
+  inline Integer &operator++()
   {
     ++value;
     return *this;
@@ -20,7 +20,7 @@ public:
   // n++
   // 1. 객체를 값으로 반환합니다. => 임시 객체 복사
   // 2. 현재 상태를 복사하는 복사 생성 비용
-  Integer operator++(int)
+  inline Integer operator++(int)
   {
     Integer temp(*this); // 현재 상태를 복사합니다.
     ++value;
@@ -34,6 +34,45 @@ public:
   }
 };
 
+int main()
+{
+  int n = 0;
+  ++n;
+  n++;
+  /*
+        mov     DWORD PTR n$[rsp], 0
+
+        mov     eax, DWORD PTR n$[rsp]
+        inc     eax
+
+        mov     DWORD PTR n$[rsp], eax
+        mov     eax, DWORD PTR n$[rsp]
+        inc     eax
+
+        mov     DWORD PTR n$[rsp], eax
+  */
+
+  Integer n2 = 0; // Integer::Integer()
+  ++n2;           // operator++()
+  n2++;           // operator++(int)
+  /*
+        mov     DWORD PTR n2$[rsp], 0
+        mov     eax, DWORD PTR n2$[rsp]
+        inc     eax
+
+        mov     DWORD PTR n2$[rsp], eax
+        mov     eax, DWORD PTR n2$[rsp]
+        mov     DWORD PTR temp$1[rsp], eax
+        mov     eax, DWORD PTR n2$[rsp]
+        inc     eax
+        mov     DWORD PTR n2$[rsp], eax
+        mov     eax, DWORD PTR temp$1[rsp]
+        mov     DWORD PTR $T2[rsp], eax
+
+  */
+}
+
+#if 0
 int main()
 {
   int n1 = 0;
@@ -62,3 +101,4 @@ int main()
   cout << n2 << ", " << r2 << endl;
   // 일반함수: ostream& operator<<(ostream& os, const Integer& n)
 }
+#endif
