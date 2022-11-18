ed2082476ac126e59b0c5f1a8b7b25b35eeeee61
Chansik Yun <chansigi@gmail.com>
2022-11-18 11:27:22 +0900

    .

--- 33_연산자오버로딩.cpp
+++ 33_연산자오버로딩.cpp
@@ -71,6 +71,7 @@ public:
 #endif
 
   friend Point operator+(const Point &lhs, const Point &rhs);
+  friend Point operator+(const Point &lhs, int a);
 };
 
 Point operator+(const Point &lhs, const Point &rhs)
@@ -82,10 +83,34 @@ Point operator+(const Point &lhs, const Point &rhs)
 //  : 객체를 대상으로 연산자를 사용할 경우, 약속된 이름의 멤버 함수 또는 일반 함수가
 //    호출됩니다.
 
+// 1. 연산자 우선순위는 변경되지 않습니다.
+// 2. 피연산자의 개수도 변경되지 않습니다.
+// 3. 새로운 연산자도 제공할 수 없습니다.
+// 4. 연산자 오버로딩이 불가능한 연산자도 있습니다.
+//  ::(범위 연산자) / .(접근 연산자) / .*(멤버 함수 포인터 접근 연산자)
+//  ?:(3항 연산자)
+// 5. 반드시 피연산자가 사용자 정의 타입이 있어야 합니다.
+//   int operator+(int a, int b) {} => 제공 불가능합니다.
+// Point operator+(const Point& lhs, int a) => 제공 가능합니다.
+Point operator+(const Point &lhs, int a)
+{
+  return Point(lhs.x + a, lhs.y + a);
+}
+// 6. 반드시 멤버 함수를 통해서만 제공되어야 하는 연산자도 있습니다.
+// => 클래스의 제작자만 제공할 수 있는 연산자 재정의 함수가 있습니다.
+//   1) 대입 연산자:       =
+//   2) 호출 연산자:       ()
+//   3) 임의 접근 연산자:   []
+//   4) 객체 포인터 연산자:  ->
+//   5) 포인터 참조 연산자:  *
+
 int main()
 {
   Point p1(10, 20);
   Point p2(30, 40);
+
+  Point p3 = p1 + 10;
+  p3.Print();
 
   Point result = p1 + p2;
   result.Print();
